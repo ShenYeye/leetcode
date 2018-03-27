fc1f458719e6d554fb4d5a8f01eefac8fcca9ae5
gaoli <820016544@qq.com>
2018-03-27 14:40:09 +0800

    6-zigzag

---
+++ 6-Zigzag-ac.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1) return s;
+        if(s.empty()) return "";
+        vector<string> row(numRows, "");
+        int len = s.length(), i = 0;
+        while(i < len){
+            for(int j = 0; j < numRows && i < len; j++,i++)
+                row[j].push_back(s[i]);
+            for(int j = numRows - 2;j > 0 &&i < len; j--,i++)
+                row[j].push_back(s[i]);
+        }
+        string result;
+        for(string r : row)
+            result += r;
+        return result;
+    }
+};
