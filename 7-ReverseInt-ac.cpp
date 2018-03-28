d463b9e1d693fab7945a0d18c3be4f3d174a9a2d
gaoli <820016544@qq.com>
2018-03-28 13:41:03 +0800

    7 Reverse Int

---
+++ 7-ReverseInt-ac.cpp
@@ -0,0 +1,25 @@
+/*
+Thinking:
+Don't forget to check overflow.
+
+long long res;
+...
+return (res > INT_MAX || res < INT_MIN) 0:res;
+
+also can do it.But it use 'long long'.
+*/
+
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x) {
+            int temp = res * 10 + x % 10;
+            if (temp / 10 != res)
+                return 0;
+            res = temp;
+            x /= 10;
+        }
+        return res;
+    }
+};
